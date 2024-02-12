f38bd3b1781b711e30d8b94d1a22b466458c834a
osho sunkanmi harry <oshosunkanmi270@gmail.com>
2024-02-12 19:10:39 +0000

    Using XML

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; 
+
+//The  namespace makes it easier to organize our application and 
+//avoid name collisions with other code 
+
+namespace HelloWorld  
+{
+
+//In.Net our program is contained within its own class 
+class Program 
+{
+    //The main function is the entry point into our app 
+    static void Main (string[] args)
+    {
+        Console.WriteLine("Hello world!");
+        Console.WriteLine("What is your name?"); 
+
+        //create a string variable ande ask the user for some input 
+        string str = Console.ReadLine(); 
+        Console.WriteLine("why, hello there" + str);
+    } 
+}
+
+}
---
+++ Project2/Project2/README.md
@@ -0,0 +1,8 @@
+
+//XML Comments are used to help provide documentation 
+// They start with triple-slashes and have a special syntax 
+// <summary> 
+// This is the main sample application function.
+// <param name<'args'> An array of string arguments from the command line</param> 
+//<returns> 
+// </returns> 
